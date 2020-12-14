810478e47428d2b3b442b4ec9e4a27556d7433de
crazykazu <63088568+crazykazu@users.noreply.github.com>
2020-12-13 22:20:29 -0800

    Update sorting.py

--- sorting.py
+++ sorting.py
@@ -2,12 +2,27 @@
 #Gonna take in a list of numbers
 #Start simple and then move my way up
 
+def bubble_sort(list):
+    #Gonna run through the whole length of the list
+    for i in range(len(list)):
+        #Keeps track of what iteration we are on and -1 so that
+        #it doesn't go out of range
+        for j in range(len(list) - i - 1):
+            #If greater than swap
+            if list[j] > list[j+1]:
+                #The swap
+                 list[j], list[j + 1] = list[j+1], list[j]
+
 def selection_sort(list):
     for i in range(len(list)):
+        #keeps track of the index that we are comparing
         min_ind = i
+        #Running through the rest of the list to find another min value
         for j in range(i + 1, len(list)):
+            #If it is less than keep track of that min value
             if(list[j] < list[min_ind]):
                 min_ind = j
+        #The swap of the min value that we found
         list[min_ind], list[i] = list[i], list[min_ind]
 
 def mergeSort(list):
@@ -38,7 +53,7 @@ def mergeSort(list):
 
 def main():
     list = [2,5,87,5,3,543,23,54,5,1,0]
-    selection_sort(list)
+    bubble_sort(list)
     print(list)
 if __name__  == '__main__':
     main()
